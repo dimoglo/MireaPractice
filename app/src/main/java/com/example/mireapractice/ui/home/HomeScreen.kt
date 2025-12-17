3246f1fb9a5c3b1ab9019b455fd28e1ede177d11
Stanislav Dimoglo <stanislav.dimoglo@dzsystems.ru>
2025-12-18 00:05:57 +0300

    исправил ошибку

--- app/src/main/java/com/example/mireapractice/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mireapractice/ui/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.mireapractice.ui.home
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -30,7 +29,6 @@ import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
@@ -43,10 +41,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -60,11 +55,9 @@ import com.example.mireapractice.ui.components.bottombar.BottomBar
 import com.example.mireapractice.ui.components.bottombar.BottomBarType
 import com.example.mireapractice.ui.components.currency_cards.CurrencyCard
 import com.example.mireapractice.ui.components.navbar.NavBar
-import com.example.mireapractice.ui.components.navbar.NavBarDefaults
 import com.example.mireapractice.ui.components.navbar.NavBarLayoutMeasurePolicy
 import com.example.mireapractice.common.utils.Constants.SIXTEEN
 import com.example.mireapractice.common.utils.Constants.TWELVE
-import com.example.mireapractice.common.utils.Constants.TWENTY_FOUR
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.ZoneId
